15a3dd129fd728e843545ca22dac85ffa9fb29c0
IuriYuri <iqbeze@gmail.com>
2023-10-17 15:10:14 -0300

    Criei a função produto, que multiplica X e Y

--- main.c
+++ main.c
@@ -6,6 +6,9 @@ float somar(float x, float y){
 float subtracao(float x, float y){
     return x-y;
 }
+float produto(float x, float y){
+    return x*y;
+}
 main(){
     float x, y, z;
     printf("digite o valor de X: ");
