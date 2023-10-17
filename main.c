2910cbb9e1a115fe0b42ebf0e741bf00e91b2298
IuriYuri <iqbeze@gmail.com>
2023-10-17 14:56:22 -0300

    Implementei a função de ler e escrever X e Y

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+main(){
+    float x, y, z;
+    printf("digite o valor de X: ");
+    scanf("%f", &x);
+    printf("digite o valor de Y: ");
+    scanf("%f", &y);
+    printf("%.1f %.1f",x,y);
+}
