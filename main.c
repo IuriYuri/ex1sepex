0203e8ba5093354e7584454054aab1895fa7c33d
IuriYuri <iqbeze@gmail.com>
2023-10-17 15:35:37 -0300

    Implementei a escolha da operação a ser realizada, assim como sua execução e as variaveis necessarias.

--- main.c
+++ main.c
@@ -14,9 +14,31 @@ float divisao(float x, float y){
 }
 main(){
     float x, y, z;
+    char sinal;
     printf("digite o valor de X: ");
     scanf("%f", &x);
     printf("digite o valor de Y: ");
     scanf("%f", &y);
+    getchar();
+    printf("digite o sinal da operação que deseja que seja realizada pelos numeros digitados.\n+ para soma\n- para subtracao\n* para multiplicacao\n / para divisao.\n");
+    sinal = getchar();
+    switch (sinal)
+    {
+    case '+':
+        z = somar(x,y);
+        break;
+    case '-':
+        z = subtracao(x,y);
+        break;
+    case '*':
+        z = produto(x,y);
+        break;
+    case '/':
+        z = divisao(x,y);
+        break;
+    default:
+        printf("valor invalido.");
+        break;
+    }
     printf("%.1f ",z);
 }
