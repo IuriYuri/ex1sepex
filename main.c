8087e0da9e3f0bb9f90904ca4389d511e624ce82
IuriYuri <iqbeze@gmail.com>
2023-10-17 15:05:29 -0300

    Criei a função soma

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+float somar(float x, float y){
+    return x + y;
+}
 main(){
     float x, y, z;
     printf("digite o valor de X: ");
     scanf("%f", &x);
     printf("digite o valor de Y: ");
     scanf("%f", &y);
-    printf("%.1f %.1f",x,y);
+    printf("%.1f ",z);
 }
